8a0807d03b9f0c3f7225be3d621c946ae1c99018
thebiggestmonstro <monstroowner@gmail.com>
2025-06-22 21:56:15 +0900

    Make AnimBlueprint

---
+++ Aura/Source/Aura/Character/AuraCharacterBase.cpp
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/AuraCharacterBase.h"
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
---
+++ Aura/Source/Aura/Character/AuraCharacterBase.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "AuraCharacterBase.generated.h"
+
+UCLASS()
+class AURA_API AAuraCharacterBase : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	AAuraCharacterBase();
+
+protected:
+	virtual void BeginPlay() override;
+
+protected:
+	/*
+	* memeber variables
+	*/
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Combat")
+	TObjectPtr<USkeletalMeshComponent> Weapon;
+};
