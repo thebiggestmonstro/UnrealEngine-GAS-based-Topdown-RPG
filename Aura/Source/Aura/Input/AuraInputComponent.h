76a242c0e185459cf0c07b57df41e5676d10eade
thebiggestmonstro <monstroowner@gmail.com>
2025-06-26 12:15:09 +0900

    Set PlayerController

---
+++ Aura/Source/Aura/Input/AuraInputComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EnhancedInputComponent.h"
+#include "AuraInputComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraInputComponent : public UEnhancedInputComponent
+{
+	GENERATED_BODY()
+	
+};
