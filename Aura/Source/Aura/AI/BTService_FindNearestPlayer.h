94a1cd2558e428121cce6fc754df6f72fe06cc38
thebiggestmonstro <monstroowner@gmail.com>
2025-09-19 20:27:16 +0900

    Override Service Class

--- Aura/Source/Aura/AI/BTService_FindNearestPlayer.h
+++ Aura/Source/Aura/AI/BTService_FindNearestPlayer.h
@@ -14,4 +14,12 @@ class AURA_API UBTService_FindNearestPlayer : public UBTService_BlueprintBase
 {
 	GENERATED_BODY()
 	
+protected:
+	virtual void TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+	FBlackboardKeySelector TargetToFollowSelector;
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+	FBlackboardKeySelector DistanceToTargetSelector;
 };
