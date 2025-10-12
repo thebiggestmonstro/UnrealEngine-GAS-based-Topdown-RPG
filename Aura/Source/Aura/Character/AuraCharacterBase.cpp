4aaa0409c60672c67cbb41294ea21c8452fac580
thebiggestmonstro <monstroowner@gmail.com>
2025-10-12 15:27:30 +0900

    Improve Elementalist Enemy AI

--- Aura/Source/Aura/Character/AuraCharacterBase.cpp
+++ Aura/Source/Aura/Character/AuraCharacterBase.cpp
@@ -154,6 +154,11 @@ int32 AAuraCharacterBase::GetMinionCount_Implementation()
 	return MinionCount;
 }
 
+void AAuraCharacterBase::IncremenetMinionCount_Implementation(int32 Amount)
+{
+	MinionCount += Amount;
+}
+
 void AAuraCharacterBase::Dissolve()
 {
 	if (IsValid(DissolveMaterialInstance))
